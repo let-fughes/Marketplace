270dd50de11ebc29be3ab030327cfeff657b99d5
Klimashevich Kiryll <alarunghez@gmail.com>
2026-04-15 11:28:05 +0300

    Created service layer for work with user profile photo.

--- userService/src/main/java/com/kirylliuss/shop/userService/service/ProfileImageService.java
+++ userService/src/main/java/com/kirylliuss/shop/userService/service/ProfileImageService.java
@@ -1,4 +1,51 @@
 package com.kirylliuss.shop.userService.service;
 
+import com.kirylliuss.shop.userService.config.MinIOConfig;
+import com.kirylliuss.shop.userService.model.User;
+import com.kirylliuss.shop.userService.repository.UserRepository;
+import io.minio.MinioClient;
+import io.minio.PutObjectArgs;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
 public class ProfileImageService {
+
+    @Value("${minio.url}")
+    private String url;
+
+    @Value("${minio.bucket}")
+    private String bucket;
+
+    private MinioClient minioClient;
+    private UserRepository userRepository;
+
+    public String uploadImage(MultipartFile file){
+        try{
+            String filename = UUID.randomUUID() + "_" + file.getOriginalFilename();
+
+            minioClient.putObject(
+                    PutObjectArgs.builder()
+                            .bucket(bucket)
+                            .object(filename)
+                            .stream(file.getInputStream(), file.getSize(), -1)
+                            .contentType(file.getContentType())
+                            .build()
+            );
+
+            return url + "/" + bucket + "/" + filename;
+        } catch (Exception ex){
+            throw  new RuntimeException("Something went wrong while uploading image with Minio: ", ex);
+        }
+    }
+
+    public String getProfilePhoto(String login){
+        User user = userRepository.findByLogin(login).orElseThrow(() -> new RuntimeException("User Not Found."));
+        return user.getProfilePhotoUrl();
+    }
 }
