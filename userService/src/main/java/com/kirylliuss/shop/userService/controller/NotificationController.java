786570c58f725d9f5fbc1b7179458de37f582b71
Klimashevich Kiryll <alarunghez@gmail.com>
2026-04-15 11:18:03 +0300

    Created v1.0 of Email Notification service.

--- userService/src/main/java/com/kirylliuss/shop/userService/controller/NotificationController.java
+++ userService/src/main/java/com/kirylliuss/shop/userService/controller/NotificationController.java
@@ -1,4 +1,32 @@
 package com.kirylliuss.shop.userService.controller;
 
+import com.kirylliuss.shop.userService.service.NotificationService;
+import jakarta.mail.MessagingException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@RestController
+@RequestMapping("/v1/api/notifications")
 public class NotificationController {
+
+    private final NotificationService notificationService;
+
+    @GetMapping("/sendEmail")
+    public ResponseEntity<String> sendEmail(Principal principal, String subject, String text) {
+        try{
+            String login = principal.getName();
+            notificationService.sendMessage(subject, text, login);
+            return ResponseEntity.ok("Message sent successfully.");
+        } catch (MessagingException ex){
+            ex.getStackTrace();
+        }
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Something went wrong while sending a message.");
+    }
+
+
 }
