a05eae2bc07a076d1795a8f0651cb16f5dcfaf0f
Klimashevich Kiryll <alarunghez@gmail.com>
2026-04-15 11:21:24 +0300

    Updated User response.

--- userService/src/main/java/com/kirylliuss/shop/userService/dto/response/UserResponse.java
+++ userService/src/main/java/com/kirylliuss/shop/userService/dto/response/UserResponse.java
@@ -11,12 +11,14 @@ import java.util.List;
 @Data
 @AllArgsConstructor
 public class UserResponse {
+    private String login;
     private String firstName;
     private String lastName;
     private LocalDate birthDate;
     private String email;
     private String phoneNumber;
     private String role;
+    private String profilePhotoUrl;
     private LocalDateTime createdAt;
     private List<Card> cards;
 }
