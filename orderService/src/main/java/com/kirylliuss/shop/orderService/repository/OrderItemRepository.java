9c5b35ecedf68e3ccb48aca5815490d38958c927
Klimashevich Kiryll <alarunghez@gmail.com>
2026-04-18 14:03:51 +0300

    Added OrderItem Repository.

---
+++ orderService/src/main/java/com/kirylliuss/shop/orderService/repository/OrderItemRepository.java
@@ -0,0 +1,13 @@
+package com.kirylliuss.shop.orderService.repository;
+
+import com.kirylliuss.shop.orderService.model.Order;
+import com.kirylliuss.shop.orderService.model.OrderItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+    List<OrderItem> findAllByOrderId(Long orderId);
+}
