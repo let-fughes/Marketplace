6175f6732fc038716b03ccc2f13918d70254e5d1
Klimashevich Kiryll <alarunghez@gmail.com>
2026-04-18 14:04:25 +0300

    Created Item mapper.

---
+++ orderService/src/main/java/com/kirylliuss/shop/orderService/mapper/ItemMapper.java
@@ -0,0 +1,21 @@
+package com.kirylliuss.shop.orderService.mapper;
+
+import com.kirylliuss.shop.orderService.dto.request.ItemRequest;
+import com.kirylliuss.shop.orderService.dto.response.ItemResponse;
+import com.kirylliuss.shop.orderService.model.Item;
+import org.hibernate.query.sqm.produce.function.StandardArgumentsValidators;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+
+@Mapper(componentModel = "spring")
+public interface ItemMapper {
+
+    @Mapping(target = "imageUrl", ignore = true)
+    Item toItem(ItemRequest itemRequest);
+
+    ItemResponse toItemResponse(Item item);
+
+    @Mapping(target = "id", ignore = true)
+    void updateItemFromRequest(ItemRequest request, @MappingTarget Item entity);
+}
