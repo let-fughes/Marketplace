b05033e4f6827790eb3e0bf095a945aa4c75d16d
Klimashevich Kiryll <alarunghez@gmail.com>
2026-04-21 14:58:07 +0300

    Added OrderItem Dto/v1.0

---
+++ orderService/src/main/java/com/kirylliuss/shop/orderService/dto/request/OrderItemRequest.java
@@ -0,0 +1,14 @@
+package com.kirylliuss.shop.orderService.dto.request;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
+public class OrderItemRequest {
+    @NotNull(message = "Item ID is required")
+    private Long itemId;
+
+    @Min(value = 1, message = "Quantity must be at least 1")
+    private Integer quantity;
+}
