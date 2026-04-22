e542027cb2c3d1f8e0339483e80e747f8df40b96
Klimashevich Kiryll <alarunghez@gmail.com>
2026-04-22 08:38:58 +0300

    Item Response Dto.

---
+++ orderService/src/main/java/com/kirylliuss/shop/orderService/dto/response/ItemResponse.java
@@ -0,0 +1,13 @@
+package com.kirylliuss.shop.orderService.dto.response;
+
+import lombok.Data;
+import java.math.BigDecimal;
+
+@Data
+public class ItemResponse {
+    private Long id;
+    private String name;
+    private String description;
+    private BigDecimal price;
+    private String category;
+}
