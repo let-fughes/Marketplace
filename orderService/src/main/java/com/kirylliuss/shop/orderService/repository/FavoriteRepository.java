93f00baebe2b50528fd1c255714f61fa44e5f16e
Klimashevich Kiryll <alarunghez@gmail.com>
2026-05-08 20:52:57 +0300

    Updated.

--- orderService/src/main/java/com/kirylliuss/shop/orderService/repository/FavoriteRepository.java
+++ orderService/src/main/java/com/kirylliuss/shop/orderService/repository/FavoriteRepository.java
@@ -2,9 +2,11 @@ package com.kirylliuss.shop.orderService.repository;
 
 import com.kirylliuss.shop.orderService.model.Favorite;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface FavoriteRepository extends JpaRepository<Long, Favorite> {
     List<Favorite> findAllByUserId(Long userId);
     void deleteByUserIdAndItemId(Long userId, Long itemId);
