3c5e46f0ed24a840beda429f51fe01c8afd32d77
Klimashevich Kiryll <alarunghez@gmail.com>
2026-05-13 23:27:07 +0300

    Added env file load logic.

--- gateway/src/main/java/com/kirylliuss/shop/gateway/ShopGatewayApplication.java
+++ gateway/src/main/java/com/kirylliuss/shop/gateway/ShopGatewayApplication.java
@@ -1,8 +1,10 @@
 package com.kirylliuss.shop.gateway;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.GatewayFilterSpec;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -12,6 +14,12 @@ import org.springframework.context.annotation.ComponentScan;
 public class ShopGatewayApplication {
 
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+
+		dotenv.entries().forEach(dotenvEntry ->
+				System.setProperty(dotenvEntry.getKey(), dotenvEntry.getValue()));
+
 		SpringApplication.run(ShopGatewayApplication.class, args);
 	}
 
@@ -19,19 +27,23 @@ public class ShopGatewayApplication {
 	public RouteLocator customGatewayRoutes(RouteLocatorBuilder builder){
 		return builder.routes()
 				.route("user-service-route", r -> r
-						.path("/v1/api/users", "/v1/api/users/**")
+						.path("/v1/api/users/**")
+						.filters(GatewayFilterSpec::tokenRelay) // Добавь это
 						.uri("http://localhost:8081"))
 				.route("auth-service", r -> r
 						.path("/v1/api/auth/**")
-						.uri("http://localhost:8082"))
+						.uri("http://localhost:8082")) // Auth обычно не нуждается в relay
 				.route("order-service-1", r -> r
 						.path("/v1/api/orders/**")
+						.filters(GatewayFilterSpec::tokenRelay) // Добавь это
 						.uri("http://localhost:8083"))
 				.route("order-service-2", r -> r
 						.path("/v1/api/items/**")
+						.filters(GatewayFilterSpec::tokenRelay) // Добавь это
 						.uri("http://localhost:8083"))
 				.route("payment-service", r -> r
 						.path("/v1/api/payments/**")
+						.filters(GatewayFilterSpec::tokenRelay) // Добавь это
 						.uri("http://localhost:8084"))
 				.build();
 	}
