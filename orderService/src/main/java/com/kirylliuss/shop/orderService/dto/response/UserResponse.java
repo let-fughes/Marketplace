476cca896119170ce71ab00f96c71b243594024e
Klimashevich Kiryll <alarunghez@gmail.com>
2026-05-13 23:44:20 +0300

    Updated response from user service.

--- orderService/src/main/java/com/kirylliuss/shop/orderService/dto/response/UserResponse.java
+++ orderService/src/main/java/com/kirylliuss/shop/orderService/dto/response/UserResponse.java
@@ -13,6 +13,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class UserResponse {
+    private Long id;
     private String firstName;
     private String lastName;
     private LocalDate birthDate;
