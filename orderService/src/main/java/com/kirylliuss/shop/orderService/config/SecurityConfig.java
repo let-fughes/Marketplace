041c8c3b7c643d03a59f2e3d54cc30f39b65c690
Klimashevich Kiryll <alarunghez@gmail.com>
2026-05-14 18:27:24 +0300

    Set up secure communication and jwt token check between order service and gateway.

--- orderService/src/main/java/com/kirylliuss/shop/orderService/config/SecurityConfig.java
+++ orderService/src/main/java/com/kirylliuss/shop/orderService/config/SecurityConfig.java
@@ -1,18 +1,31 @@
 package com.kirylliuss.shop.orderService.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtFilter;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/v1/api/items/**").permitAll()
+                        .requestMatchers("/v1/api/items").authenticated()
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(jwtFilter, org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter.class);
+
         return http.build();
     }
 }
