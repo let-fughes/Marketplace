4c16ac8c64c4afcbd51465de3c5fbc4c3532d7ae
Klimashevich Kiryll <alarunghez@gmail.com>
2026-05-14 18:24:05 +0300

    Added configuration for feign connection.

--- orderService/src/main/java/com/kirylliuss/shop/orderService/client/UserServiceClient.java
+++ orderService/src/main/java/com/kirylliuss/shop/orderService/client/UserServiceClient.java
@@ -1,17 +1,25 @@
 package com.kirylliuss.shop.orderService.client;
 
+import com.kirylliuss.shop.orderService.config.FeignConfig;
 import com.kirylliuss.shop.orderService.dto.response.UserResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.security.Principal;
 
 @FeignClient(
         name = "user-service",
         url = "${user-service.url}",
-        path = "/v1/api/users"
+        path = "/v1/api/users",
+        configuration = FeignConfig.class
 )
 public interface UserServiceClient {
     @GetMapping("/{id}")
     UserResponse getUserById(@PathVariable("id") Long id);
+
+    @GetMapping("/profileByLogin")
+    UserResponse getProfile(@RequestParam("login") String login);
 }
